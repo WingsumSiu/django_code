c628874114016a839071d4330e22fd920492f654
WingsumSiu <wingsum@hackthehood.org>
2021-07-02 19:17:22 -0700

    my_app updates

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -1,7 +1,35 @@
+# from django.urls import path
+# from django.shortcuts import get_object_or_404, render
+
+# from . import views
+
+
+# app_name = 'my_app'
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+   
+# ]
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'my_app/results.html', {'question': question})
+
+# general view
 from django.urls import path
 
 from . import views
 
+app_name = 'my_app'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
